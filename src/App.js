0e5898b103b4151cd45d47a4d830203605056ebe
Atikul Jaman <atikuljamanatikul@gmail..com>
2022-07-28 22:34:17 +0600

    Completed the Task

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
-import { useState } from 'react';
-import './App.css';
-import Countries from './components/Countries/Countries';
-import { gql, useQuery } from '@apollo/client';
-
-
-const COUNTRY_QUERY = gql`
-    query{
-      continents{
-        code
-        name
-      }
-    }
-  `
+import React from "react";
+import ReceiptForm from "./components/ReceiptForm/ReceiptForm";
+import "./App.css";
 
 function App() {
-  const [code, setCode] = useState("")
-  const { loading, error, data } = useQuery(COUNTRY_QUERY);
-
-  const handleSelectOption = e => {
-    setCode(e.target.value)
-  }
-
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
   return (
     <div className="App">
-      <h1>Continents</h1>
-      <select onChange={handleSelectOption} id='continents'>
-        <option defaultValue>select a continent</option>
-        {data?.continents?.map(continent => (
-          <option key={continent?.code} value={continent?.code}>{continent?.name}</option>
-        ))}
-      </select>
-      <Countries code={code} />
+      <ReceiptForm />
     </div>
-  )
-
+  );
 }
 
 export default App;
---
+++ src/hooks/useReceiptForm.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const useReceiptForm = () => {
+  const [receiptDetails, setReceiptDetails] = useState([]);
+  const [date, setDate] = useState("");
+  const [amount, setAmount] = useState("");
+  const [paymentMode, setPaymentMode] = useState("Cash");
+  const [remark, setRemark] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let obj = {
+      date: date,
+      amount: amount,
+      paymentMode: paymentMode,
+      remark: remark,
+    };
+
+    if (paymentMode === "") {
+      alert("Payment Mode is Required");
+    } else {
+      setReceiptDetails([...receiptDetails, obj]);
+    }
+
+    // CLEARING THE INPUT FIELDS
+    setDate("");
+    setAmount("");
+    setPaymentMode("");
+    setRemark("");
+  };
+
+  return {
+    receiptDetails,
+    date,
+    amount,
+    paymentMode,
+    remark,
+    setDate,
+    setAmount,
+    setPaymentMode,
+    setRemark,
+    handleSubmit,
+  };
+};
+
+export default useReceiptForm;
